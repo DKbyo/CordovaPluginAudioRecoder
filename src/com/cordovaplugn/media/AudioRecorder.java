c91b11ca799161cf72c6ae4fa3a7423b10cac468
JordanR <cj.rosasavila@gmail.com>
2017-08-02 23:59:08 -0500

    Update AudioRecorder.java

--- src/com/cordovaplugn/media/AudioRecorder.java
+++ src/com/cordovaplugn/media/AudioRecorder.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.Manifest;
